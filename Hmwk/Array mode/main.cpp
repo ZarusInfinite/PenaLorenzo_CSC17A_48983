c84fd8313b6dbf7aa7c20167d71de3e26d1d98cc
ZarusInfinite <zarus2009@gmail.com>
2015-10-02 15:00:00 -0700

    Changed array mode

--- Hmwk/Array mode/main.cpp
+++ Hmwk/Array mode/main.cpp
@@ -1,6 +1,6 @@
 /* 
  * File:   main.cpp
- * Author: Louli
+ * Author: Lorenzo Pena
  *
  * Created on September 27, 2015, 3:41 PM
  */
@@ -8,82 +8,103 @@
 #include <cstdlib>
 #include <iostream>
 
-void inputArray(int[],int);//Fills the array with elements inputed by user
-void sortArray(int[],int);//Function to sort our array
+void fillArray(int[],int);//Fills the array with elements inputed by user
 void displayArray(int[],int);//Function to output our array
-void modesrchArray(int[], int);//Function that determines mode within the array
+void sortArray(int[],int);
+float mean(int [],int);
+float median(int [],int);
+int *mode(int [],int);
 
 using namespace std;
 
 int main(int argc, char** argv) {
     
     int SIZE;//Hold size of array
-    cout<<"Enter size of array"<<endl;//User determines size of array
+    float average, mid;
+    cout<<"The array contains a repeated set of elements. Goes from 0 to 9 and repeats."<<endl;
+    cout<<"You choose how many elements the array holds."<<endl;//User determines size of array
     cin>>SIZE;
-    int A[SIZE];//Our array with SIZE
-    inputArray(A,SIZE);
+    int A[SIZE];//Our array
+    fillArray(A,SIZE);
     sortArray(A,SIZE);
     displayArray(A,SIZE);
-    modesrchArray(A,SIZE);
+    average=mean(A,SIZE);
+    mid=median(A,SIZE);
+    cout<<"The mean is: "<<average<<endl;
+    cout<<"The median is: "<<mid<<endl;
     return 0;
 }
 
-void inputArray(int A[], int SIZE)
+void fillArray(int A[], int SIZE)
 {
-    cout<<"Enter array elements"<<endl;
-        for(int count=0; count<SIZE; count++)
+    int element=0;
+        for(int i=0; i<SIZE; i++)
         {
-            cin>>A[count];
+            A[i]=element++;
+            if(element%10==0)
+            {
+                element=0;
+            }
         }
+}
+
+void displayArray(int A[],int SIZE)
+{
+    cout<<"This is the array sorted:"<<endl;
+    for(int i =0; i<SIZE; i++)
+    {
+        if(i%10==0)
+        {
+            cout<<endl;
+        }
+        cout<<A[i]<<" ";
+    }
+    cout<<endl;
 }
 
 void sortArray(int A[],int SIZE)
 {
-    int count, index, min;
-    for (count = 0; count <SIZE; count++)
+    int index, min;
+    for (int i = 0; i <SIZE; i++)
     {
-       index = count;
-       min= A[count];
-       for(int i = count + 1; i < SIZE; i++)
+       index = i;
+       min= A[i];
+       for(int count = i + 1; count < SIZE; count++)
        {
-          if (A[i] < min)
+          if (A[count] < min)
           {
-             min = A[i];
-             index= i;
+             min = A[count];
+             index= count;
           }
        }
-       A[index] = A[count];
-       A[count] = min;
+       A[index] = A[i];
+       A[i] = min;
     }
  }
 
-void displayArray(int A[],int SIZE)
+float mean(int A[],int SIZE)
 {
-    int count;
-    cout<<"This is the array sorted:"<<endl;
-    for(count =0; count<SIZE; count++)
+    float total=0;
+    for(int i=0; i<SIZE; i++)
     {
-        cout<<A[count]<<endl;
+        total+= A[i];
     }
+    float mean=total/SIZE;
+    return mean;
 }
 
-void modesrchArray(int A[], int size)
+float median(int A[], int SIZE)
 {
-    int max=0;//
-    int mdCount = 1;//Counts the amount of times an element is repeated
-    int mode=A[0];//The value of the mode
-    for(int count= 0; count<size; count++)
+    float tempMedian, median;
+    int *Aptr=A;
+    if(SIZE%2==0)//If there are an even number of elements
     {
-        if(A[count]==A[count+1])
-        {
-            mdCount++;
-            if(mdCount > max)
-            {
-                max= mdCount;
-                mode=A[count];
-            }
-        }
-    else mdCount=1;
+        tempMedian= *(Aptr+SIZE/2-1)+ *(Aptr+SIZE/2);
+        median=tempMedian/2;
     }
-cout << "The mode of the array is: " << mode << endl;
+    else//If there are an odd number of elements
+    {
+        median= *(Aptr+(1+SIZE)/2-1);
+    }
+       return median; 
 }
