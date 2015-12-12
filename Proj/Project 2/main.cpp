cfa5015265c3ce5c064b6d2326029ad7629873b2
ZarusInfinite <zarus2009@gmail.com>
2015-12-12 13:24:27 -0800

    added proj 2

---
+++ Proj/Project 2/main.cpp
@@ -0,0 +1,26 @@
+/* 
+ * File:   main.cpp
+ * Author: Louli
+ *
+ * Project: PC building and benchmarking program. Takes in user's budget and
+ * for custom PC. This program will choose from a database of hardware and create 
+ * a hardware list that will best satisfy the user's budget and need.
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    /********************************************************************************************************************************/
+    //GPU class
+    
+    return 0;
+}
+
